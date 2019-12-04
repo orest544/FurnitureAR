d6ecb85f80113d75f90b5fef631bdbdfcdd6f3d0
Orest Patlyka <orest.patlyka@gmail.com>
2019-12-04 17:13:31 +0200

    Added project and made initial configuration

---
+++ FurnitureAR/FurnitureARTests/FurnitureARTests.swift
@@ -0,0 +1,34 @@
+//
+//  FurnitureARTests.swift
+//  FurnitureARTests
+//
+//  Created by Orest Patlyka on 04.12.2019.
+//  Copyright © 2019 Orest Patlyka. All rights reserved.
+//
+
+import XCTest
+@testable import FurnitureAR
+
+class FurnitureARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
